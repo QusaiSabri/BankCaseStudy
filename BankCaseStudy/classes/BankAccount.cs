50a73d1d938092e921b7f91c1d5bafc7a5fa8473
Qusai Sabri <qusay.sabri19@gmail.com>
2021-03-14 16:40:12 -0500

    Initialize account

--- BankCaseStudy/classes/BankAccount.cs
+++ BankCaseStudy/classes/BankAccount.cs
@@ -8,6 +8,12 @@ namespace BankCaseStudy.classes
     public string Owner { get; set; }
     public decimal Balance { get; set; }
 
+    public BankAccount(string name, decimal initialBalance)
+    {
+        this.Owner = name;
+        this.Balance = initialBalance;
+    }
+
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
     }
