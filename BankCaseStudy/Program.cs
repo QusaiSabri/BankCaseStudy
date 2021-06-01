b3472f3d1cf18c78d6ce8dab0906e11f1926d4f8
Qusai Sabri <qusay.sabri19@gmail.com>
2021-06-01 00:17:11 -0500

    Add transaction logs & implementation details for the Deposit and Withdrawal methods

--- BankCaseStudy/Program.cs
+++ BankCaseStudy/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using BankCaseStudy.classes;
+using classes;
 
 namespace BankCaseStudy
 {
@@ -11,6 +11,41 @@ namespace BankCaseStudy
             Console.WriteLine(" ");
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
             Console.WriteLine(" ");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine("Transactions log");
+            Console.WriteLine(" ");
+            Console.WriteLine(account.GetAccountHistory());
+            Console.WriteLine(" ");
+
+
+            // Test that the initial balances must be positive.
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
